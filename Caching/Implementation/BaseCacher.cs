68c362e48566ee785083965cd7842eb644f65a37
unknown <armand@Armand-PC.comprsa.local>
2013-12-24 12:46:42 +0200

    Initial

---
+++ Caching/Implementation/BaseCacher.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+namespace Yotta.Caching
+{
+    public enum CacherPriority
+    {
+        Default,
+        NotRemovable
+    }
+    public delegate Object Function(dynamic list);
+    public abstract class BaseCacher
+    {
+        protected abstract void AddToMyCache(String CacheKeyName, Object CacheItem, CacherPriority Priority);
+        protected abstract Object GetChaceItem(String CacheKeyName);
+        protected abstract void RemoveCacheItem(String CacheKeyName);
+        public Object Invoke(Function theFunction, dynamic list)
+        {
+            String key = theFunction.ToString() + new JavaScriptSerializer().Serialize(list);
+
+            Object result = this.GetChaceItem(key);
+
+            if (result == null)
+            {
+                result = theFunction(list);
+                this.AddToMyCache(key, result, CacherPriority.Default);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ TestCacher/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Yotta.Caching;
+
+namespace TestCacher
+{
+    class Program
+    {
+        public static object DoCalculation(dynamic args)
+        {
+            return args.i * args.p;
+        }
+
+        public static string ThisIsATest(dynamic args)
+        {
+            return "This is a test " + args.message;
+        }
+
+        static void Main(string[] args)
+        {
+            BaseCacher cacher = new BasicCacher();
+
+            var result = cacher.Invoke(new Function(DoCalculation), new { i = 1, p = 2.0 });
+            Console.WriteLine(result);
+
+            result = cacher.Invoke(new Function(DoCalculation), new { i = 1, p = 2.0 });
+            Console.WriteLine(result);
+
+            result = cacher.Invoke(new Function(DoCalculation), new { i = 2, p = 2.0 });
+            Console.WriteLine(result);
+
+            result = cacher.Invoke(new Function(DoCalculation), new { i = 1, p = 2.0 });
+            Console.WriteLine(result);
+
+            Console.WriteLine(cacher.Invoke(new Function(ThisIsATest), new { message = "Hallo" }));
+            Console.WriteLine(cacher.Invoke(new Function(ThisIsATest), new { message = "Hallo 2" }));
+            Console.WriteLine(cacher.Invoke(new Function(ThisIsATest), new { message = "Hallo" }));
+
+            Console.Read();
+        }
+    }
+}
